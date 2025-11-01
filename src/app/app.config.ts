10c166a01fea4a66ad7c4dd1a773be0f4a7a5fde
PabloG02 <devpablogg@gmail.com>
2025-11-01 12:47:08 +0100

    Add ECharts and HTTP client providers to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideEchartsCore({ echarts }),
   ]
 };
