e76f0d5cd95032eca3b79b5d4789660bdd41ed4d
PabloG02 <devpablogg@gmail.com>
2025-11-08 13:28:41 +0100

    Replace Material Icons with Material Symbols

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideEchartsCore } from 'ngx-echarts';
+import { MatIconRegistry } from '@angular/material/icon';
 import * as echarts from 'echarts';
 
 import { routes } from './app.routes';
@@ -13,5 +19,15 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideEchartsCore({ echarts }),
+    provideAppInitializer(() => {
+      const initializerFn = ((iconRegistry: MatIconRegistry) => () => {
+        const defaultFontSetClasses = iconRegistry.getDefaultFontSetClass();
+        const outlinedFontSetClasses = defaultFontSetClasses
+          .filter((fontSetClass) => fontSetClass !== 'material-icons')
+          .concat(['material-symbols-rounded']);
+        iconRegistry.setDefaultFontSetClass(...outlinedFontSetClasses);
+      })(inject(MatIconRegistry));
+      return initializerFn();
+    })
   ]
 };
