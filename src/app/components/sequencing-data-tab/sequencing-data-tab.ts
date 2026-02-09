c402e7f19d5c6a7867456dc0cf0a64460000e0c3
PabloG02 <devpablogg@gmail.com>
2026-02-09 08:57:54 +0100

    Display base distribution percentages in Sequencing Data tab

--- src/app/components/sequencing-data-tab/sequencing-data-tab.ts
+++ src/app/components/sequencing-data-tab/sequencing-data-tab.ts
@@ -9,6 +9,7 @@ import {MatFormField, MatLabel} from '@angular/material/input';
 import {MatOption, MatSelect} from '@angular/material/select';
 import {NgxEchartsDirective} from 'ngx-echarts';
 import {FormField, form, required} from '@angular/forms/signals';
+import {DecimalPipe, KeyValuePipe} from '@angular/common';
 
 @Component({
   selector: 'app-sequencing-data-tab',
@@ -26,7 +27,9 @@ import {FormField, form, required} from '@angular/forms/signals';
     MatCardTitle,
     NgxEchartsDirective,
     FormField,
-    MatOption
+    MatOption,
+    KeyValuePipe,
+    DecimalPipe
   ],
   templateUrl: './sequencing-data-tab.html',
   styleUrl: './sequencing-data-tab.scss'
@@ -43,6 +46,40 @@ export class SequencingDataTab {
 
     // Flatten all keys across size maps
     return Object.values(accepted).flatMap(sizeMap => Object.keys(sizeMap).map(Number));
+  });
+
+  readonly baseDistributionPercentages = computed(() => {
+    const report = this.experimentReport();
+    if (!report) return null;
+
+    const cycleName = report.selectionCycleResponse.name;
+    const acceptedByCycle = report.metadata.nucleotideDistributionAccepted[cycleName];
+    if (!acceptedByCycle) return null;
+
+    let adenineTotal = 0;
+    let cytosineTotal = 0;
+    let guanineTotal = 0;
+    let thymineTotal = 0;
+
+    for (const sizeBucket of Object.values(acceptedByCycle)) {
+      for (const positionCounts of Object.values(sizeBucket)) {
+        adenineTotal += positionCounts[65] ?? 0;
+        cytosineTotal += positionCounts[67] ?? 0;
+        guanineTotal += positionCounts[71] ?? 0;
+        thymineTotal += positionCounts[84] ?? 0;
+      }
+    }
+
+    const totalCount = adenineTotal + cytosineTotal + guanineTotal + thymineTotal;
+    if (totalCount === 0) return null;
+
+    const toPercent = (value: number) => ((value / totalCount) * 100);
+    return {
+      A: toPercent(adenineTotal),
+      C: toPercent(cytosineTotal),
+      G: toPercent(guanineTotal),
+      T: toPercent(thymineTotal)
+    };
   });
 
   /* State */
